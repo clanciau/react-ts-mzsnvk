61a0e482fa5f2b05f01370afbf1bf14ba5dbf34c
clanciau <christophe.lanciau@free.fr>
2020-06-26 08:47:34 +0200

    1 file modified

--- Hello.tsx
+++ Hello.tsx
@@ -1,3 +1,9 @@
 import React from 'react';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+//export default ({ name }) => <h1>Hello {name}!</h1>;
+export default ({ name }) =>
+  <form>
+    <label >name:</label>
+    <input type="text" value={name}></input>
+    <input type="submit" ></input>
+  </form>
